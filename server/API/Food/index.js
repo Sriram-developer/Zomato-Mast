2de041dd48da2f64a24f8040f4de5f85457decfa
SRIRAM <Sriram>
2021-08-20 02:22:16 +0530

    Add menu route

--- server/API/Food/index.js
+++ server/API/Food/index.js
@@ -8,9 +8,36 @@ import { FoodModel } from "../../database/allModels";
 const Router = express.Router();
 
 /*
-Route   :  /
-Des     :  Get all the restaurant details based in city
-params  :  None
+Route   :  /r
+Des     :  Get all food based on particular restaurant
+params  :  id
 Access  :  Public
 Method  :  GET
 */
+Router.get("/r/:_id", async (req, res) => {
+  try {
+      const { _id } = req.params;
+      const foods = await FoodModel.find({ restaurant: _id });
+      return res.json({ foods });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+});
+
+/*
+Route   :  /r
+Des     :  Get all food based on particular category
+params  :  category
+Access  :  Public
+Method  :  GET
+*/
+Router.get("/r/:category", async (req, res) => {
+   try {
+        const { category } = req.params;
+        const foods = await FoodModel.find({ 
+           category: { $regex: category, $options: "i" } });
+        return res.json({ foods });
+      } catch (error) {
+        return res.status(500).json({ error: error.message });
+      }
+  });
