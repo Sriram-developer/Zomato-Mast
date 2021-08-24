05a3c0a8bdb23ae564a73bf7e81be5d16d38bbc3
SRIRAM <Sriram>
2021-08-25 01:45:46 +0530

    Add user route

--- server/API/Image/index.js
+++ server/API/Image/index.js
@@ -1,24 +1,19 @@
 // Libraries
 import express from "express";
 import passport from "passport";
-import AWS from "aws-sdk";
 import multer from "multer";
 
 // Database model
 import { ImageModel } from "../../database/allModels";
+
+// Utilities
+import { s3Upload } from "../../Utils/AWS/s3";
 
 const Router = express.Router();
 
 // Multer Config
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
-
-// AWS s3 bucket config
-const s3Bucket = new AWS.S3({
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_S3_SECRET_KEY,
-    region: "ap-south-1",
-});
 
 /*
 Route   :  /
@@ -37,16 +32,7 @@ Router.post("/", upload.single("file"), async (req, res) => {
           Key: file.originalname,
           Body: file.buffer,
           ContentType: file.mimetype,
-          ACL: "public-read", // Access Control List
-      };
-
-      const s3Upload = (options) =>{
-          return new Promise((resolve, reject) => 
-            s3Bucket.upload(options, (error, data) => {
-                if(error) return reject(error);
-                return resolve(data);
-          })
-        );
+          ACL:"public-read", // Access Control List
       };
 
       const uploadImage = await s3Upload(bucketOptions);
