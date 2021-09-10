b86b33d83a575d73c1bb35652f1afc474daf4b18
SRIRAM <Sriram>
2021-09-10 16:21:20 +0530

    Add Dining Carousal

---
+++ client/src/Components/Dining/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Components
+import DiningCarousal from "./DiningCarousal";
+
+const Dining = () => {
+    return (
+        <div>
+          <DiningCarousal/>  
+        </div>
+    );
+};
+
+export default Dining;
--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -3,13 +3,14 @@ import { useParams } from "react-router-dom";
 
 // Components
 import Delivery from "./Delivery";
-import DeliveryCarousel from "./Delivery/DeliveryCarousel";
+import Dining from "./Dining";
 
 const Master = () => {
     const { type }= useParams();
     return (
         <div className="my-5">
         { type === "delivery" && <Delivery/> }
+        { type === "dining" && <Dining/> }
         </div>
     );
 }
