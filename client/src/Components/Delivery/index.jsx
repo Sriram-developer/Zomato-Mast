a452f4a130174d397d02172b45b97c1d84e1c45b
SRIRAM <Sriram>
2021-09-10 14:49:34 +0530

    Add Delivery section

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Components
 import DeliveryCarousel from "./DeliveryCarousel";
@@ -6,11 +6,58 @@ import Brand from "./Brand";
 import RestaurantCard from "../RestaurantCard";
 
 const Delivery = () => {
+    const [restaurantList, setRestaurantList] = useState([
+        {
+            _id: "123456",
+            photos: [
+             "https://b.zmtcdn.com/data/pictures/7/55397/fb83f44f6bbbdd08809b95f8fb6bdb98.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+            ],
+            name:"Empire Restaurant",
+            cuisine: ["Street Food", "Beverages","Tea"],
+            averageCost: 100,
+            ispro: true,
+            isoff: 80,
+            durationOfdelivery: 47,
+            restaurantReviewValue: 4.1,
+        },
+        {
+            _id: "123456-2",
+            photos: [
+             "https://b.zmtcdn.com/data/pictures/2/18718102/83ead0802e9477bab63cce791fe0e778.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+            ],
+            name:"Sardarji Londonwaley",
+            cuisine: ["koramangalam", "Beverages","bangalore"],
+            averageCost: 100,
+            ispro: true,
+            isoff: 80,
+            durationOfdelivery: 47,
+            restaurantReviewValue: 4.1,
+        },
+        {
+            _id: "123456-3",
+            photos: [
+             "https://b.zmtcdn.com/data/pictures/chains/4/50574/9410759d611db9c62c3acc23c1f27e06_o2_featured_v2.jpg",
+            ],
+            name:"KFC",
+            cuisine: ["Burger", "Beverages","Sandwich"],
+            averageCost: 100,
+            ispro: true,
+            isoff: 80,
+            durationOfdelivery: 47,
+            restaurantReviewValue: 4.1,
+        },
+    ]);
     return (
         <>
             <DeliveryCarousel/>
             {/*<Brand/>*/}
-            <RestaurantCard/>
+            <div className="flex justify-between flex-wrap">
+            {
+                restaurantList.map((restaurant) =>(
+                    <RestaurantCard { ...restaurant } key={ restaurant._id }/>
+                ))
+            }
+           </div>
         </>
     );
 };
--- client/src/Components/RestaurantCard.jsx
+++ client/src/Components/RestaurantCard.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 import { AiTwotoneStar } from "react-icons/ai";
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
     return (
         <>
-          <div className="bg-white p-2 w-full mb-20 rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/3">
+          <div className="bg-white p-4 mb-4 w-full rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/3">
               <div className="w-full h-56 lg:h-64 relative">
                   <div className="absolute w-full bottom-4 flex items-end justify-between">
                   <div className="flex flex-col gap-2 items-start">
-                  <span  className="bg-zomato-400 text-white px-2 py-1 rounded text-sm">pro extra 10% off</span>
-                  <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">₹80 OFF</span>
+                      {
+                          props.ispro && (
+                            <span  className="bg-zomato-400 text-white px-2 py-1 rounded text-sm">pro extra 10% off</span>
+                          )
+                      }
+                      {
+                          props.isoff && (
+                            <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">₹{ `${ props.isoff }`} OFF</span>
+                          )
+                      }
                   </div>
-                  <span className="bg-white bg-opacity-75 p-1 rounded mr-3">42 min</span>
+                  <span className="bg-white bg-opacity-75 p-1 rounded mr-3">{ props.durationOfdelivery } min</span>
                   </div>
-                  <img src="https://b.zmtcdn.com/data/pictures/chains/2/18649542/c2d3bdda7a4a5fd097ceb37f3e0e5427_o2_featured_v2.jpg" alt="food"
+                  <img src={ props.photos.length && props.photos[0] } alt="food"
                    className="w-full h-full rounded-2xl" />
               </div>
               <div className="my-2 flex flex-col gap-2">
                   <div className="flex items-center justify-between"> 
-                      <h4 className="text-xl font-medium">Havmor Ice Cream</h4>
-                      <span className="bg-green-800 text-white text-sm p-1 rounded flex items-center">4.1 <AiTwotoneStar/></span>
+                      <h4 className="text-xl font-medium">{ props.name }</h4>
+                      <span className="bg-green-800 text-white text-sm p-1 rounded flex items-center">{ props.restaurantReviewValue } <AiTwotoneStar/></span>
                   </div>
                   <div className="flex items-center justify-between text-gray-500">
-                      <p>Desert IceCream, Beverage ,Banglore. </p>
-                      <p>₹350</p>
+                      <p> { props.cuisine.join(", ") }</p>
+                      <p>{ props.averageCost } for one</p>
                   </div>
               </div>
           </div>  
