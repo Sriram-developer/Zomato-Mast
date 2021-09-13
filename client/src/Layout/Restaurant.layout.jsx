650bc6c694301dfa9838041a776aac867c4ab5ef
SRIRAM <Sriram>
2021-09-14 00:04:35 +0530

    Add Restaurant Layout

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { TiStarOutline } from "react-icons/ti";
+import { RiDirectionLine, RiShareForwardLine } from "react-icons/ri";
+import { BiBookmarkPlus } from "react-icons/bi";
 
 // components
 import RestaurantNavbar from "../Components/Navbar/RestaurantNavbar";
 import ImageGrid from "../Components/restaurant/ImageGrid";
+import RestaurantInfo from "../Components/restaurant/RestaurantInfo";
+import InfoButtons from "../Components/restaurant/InfoButtons";
 
 const RestaurantLayout = () => {
   return (
@@ -19,6 +24,27 @@ const RestaurantLayout = () => {
             "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
           ]}
         />
+        <RestaurantInfo 
+        name="Madurai Xpress" 
+        restaurantRating="4.1" 
+        deliveryRating="3.5"
+        cuisine="North Indian, FastFood, Chinese, Street Food"
+        address="Kalavasal, Madurai."
+         />
+         <div className="my-4 flex flex-wrap gap-3 ">
+          <InfoButtons isActive>
+              <TiStarOutline/>Add Review
+          </InfoButtons>
+          <InfoButtons>
+          <RiDirectionLine/>Direction
+          </InfoButtons>
+          <InfoButtons>
+              <BiBookmarkPlus/>Bookmark
+          </InfoButtons>
+          <InfoButtons>
+              <RiShareForwardLine/>Share
+          </InfoButtons>
+         </div>
       </div>
     </>
   );
