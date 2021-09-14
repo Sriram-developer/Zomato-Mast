ab217f3d76477c6d02fa56af3e377945ac962f16
SRIRAM <Sriram>
2021-09-14 15:01:17 +0530

    Add Open Street Map

--- client/src/Components/temp.jsx
+++ client/src/Components/temp.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
  
 const Temp = () => {
+    const loc = useLocation();
     return (
         <div>
-           
+           Hello this is temp file component { loc.pathname }
         </div>
     );
 };
