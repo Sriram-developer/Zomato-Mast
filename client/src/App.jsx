2be3d9e4c42eab40ab7eae8dd9e33705cdd56d05
SRIRAM <Sriram>
2021-09-20 23:47:14 +0530

    Add Food List

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@ import { Route, Redirect } from "react-router-dom";
 //HOC
 import HomeLayoutHOC from "./HOC/Home.Hoc";
 import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
-
-// Component
-import Temp from "./Components/temp";
+import CheckoutLayoutHOC from "./HOC/Checkout.Hoc";
 
 // Pages
 import Home from "./Page/Home";
@@ -13,6 +11,7 @@ import OrderOnline from "./Page/Restaurant/OrderOnline";
 import Reviews from "./Page/Restaurant/Reviews";
 import Menu from "./Page/Restaurant/Menu";
 import Photos from "./Page/Restaurant/Photos";
+import Checkout from "./Page/Checkout";
 
 function App() {
   return (
@@ -30,6 +29,7 @@ function App() {
      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={ Menu }/>
      <RestaurantLayoutHOC path="/restaurant/:id/reviews" exact component={ Reviews }/>
      <RestaurantLayoutHOC path="/restaurant/:id/photos" exact component={ Photos }/>
+     <CheckoutLayoutHOC   path="/checkout/orders" exact component={ Checkout }/>
     </>
   );
 }
